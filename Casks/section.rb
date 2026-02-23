9e1624ef7e2ded0751286f6d6dcf82a71cbe2d21
keypad <githubs15@outlook.com>
2026-02-23 15:46:05 +0000

    □ simplify dmg background to a minimal gradient

--- Casks/section.rb
+++ Casks/section.rb
@@ -1,6 +1,6 @@
 cask "section" do
   version "0.1.0"
-  sha256 "e1bf92b5a02d3927e5022af46e7987063199934c99946156eb6980eb83105522"
+  sha256 "def032512d974c51dbe751d8b65a2b6347f1c036d8b290841c0355023b1db737"
 
   url "https://github.com/keypad/section/releases/download/v#{version}/section-#{version}.dmg"
   name "Section"
