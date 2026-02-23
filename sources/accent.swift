3b723147eb2ab252c9d13b9df9643093d98038d3
keypad <githubs15@outlook.com>
2026-02-23 09:28:10 +0000

    □ define full palette roles for each accent set using curated theme values

--- sources/accent.swift
+++ sources/accent.swift
@@ -11,17 +11,77 @@ enum Accent: Int {
 	var color: Color {
 		switch self {
 		case .warm:
-			return Color(red: 0.832, green: 0.69, blue: 0.549)
+			return Color(hex: 0xD4B08C)
 		case .catppuccin:
-			return Color(red: 0.796, green: 0.651, blue: 0.969)
+			return Color(hex: 0xCBA6F7)
 		case .nord:
-			return Color(red: 0.533, green: 0.753, blue: 0.816)
+			return Color(hex: 0x88C0D0)
 		case .tokyonight:
-			return Color(red: 0.478, green: 0.682, blue: 1.0)
+			return Color(hex: 0x7AA2F7)
 		case .gruvbox:
-			return Color(red: 0.984, green: 0.741, blue: 0.247)
+			return Color(hex: 0xFABD2F)
 		case .solarized:
-			return Color(red: 0.149, green: 0.545, blue: 0.824)
+			return Color(hex: 0x268BD2)
 		}
+	}
+
+	var card: Color {
+		switch self {
+		case .warm:
+			return Color(hex: 0x0F1218)
+		case .catppuccin:
+			return Color(hex: 0x313244)
+		case .nord:
+			return Color(hex: 0x3B4252)
+		case .tokyonight:
+			return Color(hex: 0x24283B)
+		case .gruvbox:
+			return Color(hex: 0x3C3836)
+		case .solarized:
+			return Color(hex: 0x073642)
+		}
+	}
+
+	var bar: Color {
+		switch self {
+		case .warm:
+			return Color(hex: 0x090B10)
+		case .catppuccin:
+			return Color(hex: 0x45475A)
+		case .nord:
+			return Color(hex: 0x434C5E)
+		case .tokyonight:
+			return Color(hex: 0x292E42)
+		case .gruvbox:
+			return Color(hex: 0x504945)
+		case .solarized:
+			return Color(hex: 0x586E75)
+		}
+	}
+
+	var text: Color {
+		switch self {
+		case .warm:
+			return Color.white.opacity(0.9)
+		case .catppuccin:
+			return Color(hex: 0xCDD6F4)
+		case .nord:
+			return Color(hex: 0xECEFF4)
+		case .tokyonight:
+			return Color(hex: 0xC0CAF5)
+		case .gruvbox:
+			return Color(hex: 0xEBDBB2)
+		case .solarized:
+			return Color(hex: 0x93A1A1)
+		}
+	}
+}
+
+extension Color {
+	init(hex: UInt32) {
+		let red = Double((hex >> 16) & 0xff) / 255
+		let green = Double((hex >> 8) & 0xff) / 255
+		let blue = Double(hex & 0xff) / 255
+		self.init(red: red, green: green, blue: blue)
 	}
 }
