7d8e31b3525159dfa1197ac7d38e4eebf5645559
keypad <githubs15@outlook.com>
2026-02-23 15:52:49 +0000

    □ update cask checksum for rebuilt minimal dmg

--- Casks/section.rb
+++ Casks/section.rb
@@ -1,6 +1,6 @@
 cask "section" do
   version "0.1.0"
-  sha256 "def032512d974c51dbe751d8b65a2b6347f1c036d8b290841c0355023b1db737"
+  sha256 "919c2f7c8f217b88556d6b9fd4a7c3920ed01c95f71ef2ea31fcc9f05aa0e4ea"
 
   url "https://github.com/keypad/section/releases/download/v#{version}/section-#{version}.dmg"
   name "Section"
